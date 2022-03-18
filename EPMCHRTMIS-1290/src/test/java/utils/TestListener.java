fa08da021a41e5747a47905d3c2c49060d0a1922
Kirill Surazhev <surazhevkirillleonidovich@gmail.com>
2022-03-18 10:49:38 +0300

    Add screenshots

--- EPMCHRTMIS-1290/src/test/java/utils/TestListener.java
+++ EPMCHRTMIS-1290/src/test/java/utils/TestListener.java
@@ -1,10 +1,25 @@
 package utils;
 
+import com.codeborne.selenide.WebDriverRunner;
+import com.epam.reportportal.message.ReportPortalMessage;
+import com.google.common.io.BaseEncoding;
+import com.google.common.io.Resources;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class TestListener implements ITestListener {
 
@@ -12,19 +27,20 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult result) {
-
         log.info("Test [" + result.getName() + "] start");
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         log.info("Test [" + result.getName() + "] passed successfully");
+        saveScreenshot("success");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         log.error("Test [" + result.getName() + "] failed");
         log.error(result.getThrowable());
+        saveScreenshot("failure");
     }
 
     @Override
@@ -45,5 +61,37 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onFinish(ITestContext context) {
+    }
+
+    private void saveScreenshot(String result) {
+        File screenCapture = ((TakesScreenshot) WebDriverRunner
+                .getWebDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenCapture, new File(
+                    "src/main/resources/"
+                            +
+                            "screenshot.png"));
+        } catch (IOException e) {
+            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+        }
+
+        String screenshot = "screenshot.png";
+
+        try {
+            log.info(
+                    "RP_MESSAGE#BASE64#{}#{}",
+                    BaseEncoding.base64().encode(Resources.asByteSource(Resources.getResource(screenshot)).read()),
+                    "on test " + result + " screenshot"
+
+            );
+            FileUtils.delete(new File(
+                    "src/main/resources/"
+                            +
+                            "screenshot.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
